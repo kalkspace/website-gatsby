124f95d2cb54109d9efcd7dafa86fbcee0dd7e60
Till <32832863+fancyn3rd@users.noreply.github.com>
2022-03-01 22:59:27 +0100

    Add generative star background (#2)
    * Add generative star background
    
    * Refactor
    
    * Show with content
    
    Co-authored-by: Till <->
    Co-authored-by: Marcus Weiner <marcus.weiner@gmail.com>

---
+++ src/components/stars/stars.stories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { GenerativeStarBackground } from "./stars";
+import StyleGuide from "../../styles/styleguide.mdx";
+
+export default {
+  title: "GenerativeStarBackground",
+  component: GenerativeStarBackground,
+};
+
+const Template = (args) => <GenerativeStarBackground {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {}
+
+export const WithContent = Template.bind({});
+WithContent.args = {
+  children: <StyleGuide />
+}
