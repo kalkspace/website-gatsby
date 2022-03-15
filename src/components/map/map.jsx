b50d4de5b778d6ad15e5a27d9d11fc9caed6f384
Marcus Weiner <marcus.weiner@gmail.com>
2022-03-15 22:16:31 +0100

    Create a styled map

---
+++ src/components/map/map.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { icon } from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import logoSkizze from "../../images/logo_skizze_weiss.svg";
+
+var logoIcon = icon({
+  iconUrl: logoSkizze,
+  iconSize: [38, 38],
+  popupAnchor: [0, -19],
+});
+
+/** @type {React.FC<{
+  position: [number, number],
+  zoom: number,
+  scrollWheelZoom?: boolean,
+  mapClassName?: string,
+  popupText?: string,
+}>} */
+export const Map = ({
+  position,
+  zoom,
+  mapClassName,
+  scrollWheelZoom,
+  popupText,
+}) => {
+  return (
+    <MapContainer
+      center={position}
+      zoom={zoom}
+      scrollWheelZoom={scrollWheelZoom}
+      className={mapClassName}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={logoIcon}>
+        {popupText && <Popup>{popupText}</Popup>}
+      </Marker>
+    </MapContainer>
+  );
+};
---
+++ src/components/map/map.stories.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Map } from "./map";
+import styles from"./map.stories.module.css"
+
+export default {
+  title: "Map",
+  component: Map,
+};
+
+/** @type {import('@storybook/react').Story<React.ComponentProps<import('./map').Map>>} */
+const Template = (args) => (
+  <div style={{ height: "400px" }}>
+    <Map {...args} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  position: [50.940569, 7.019904],
+  zoom: 16,
+  mapClassName: styles.mapContainer,
+  scrollWheelZoom: false,
+};
+
+export const WithPopup = Template.bind({});
+WithPopup.args = {
+  position: [50.940569, 7.019904],
+  zoom: 16,
+  mapClassName: styles.mapContainer,
+  scrollWheelZoom: false,
+  popupText: "Innenhof, dann nach rechts. Dritte Tür."
+};
