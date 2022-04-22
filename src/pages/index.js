3bd0148835496a055cd96d347b226dd1341faa45
christinegotth <christine.gotth@gmail.com>
2022-04-22 15:51:37 +0200

    Add content to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,13 @@ import blogImg from "../images/rc3-robots.jpg";
 import roboImg from "../images/robot.png";
 import blumeImg from "../images/blume.png";
 import space1 from "../images/kalkspace_sternenposter.jpg";
+import railsloveLogo from "../images/railslove_logo.png";
+import stormforgerLogo from "../images/stormforger_logo.png";
+import penny from "../images/penny.jpg";
+import marcus from "../images/marcus.jpg";
+import mop from "../images/mop.jpg";
+import christine from "../images/christine.png";
+
 import * as styles from "./index.module.css";
 
 const IndexPage = () => {
@@ -138,8 +145,99 @@ const IndexPage = () => {
           </ul>
           <Button label="Häh?" url="https://google.com" />
         </ContentBox>
-        <ContentBox sideImage={<img src={blumeImg} alt="Blumen Grafik" />}>
+        <ContentBox
+          mode="Center"
+          sideImage={<img src={blumeImg} alt="Blumen Grafik" />}
+        >
           <h2>Partner*innen</h2>
+          <img
+            src={railsloveLogo}
+            alt="Railslove Logo"
+            className={styles.partnerLogo}
+          />
+          <p>
+            Unsere Freund*innen von{" "}
+            <a href="https://railslove.com">Railslove</a> haben schon früh an
+            unsere Ideen geglaubt und unterstützen uns seitdem auf ganzer Linie.
+            Danke!
+          </p>
+          <img
+            src={stormforgerLogo}
+            alt="Stormforger Logo"
+            className={styles.partnerLogo}
+          />
+          <p>
+            Dank <a href="https://www.stormforger.com/">StormForger</a> haben
+            wir einen vernünftigen Drucker in unserem Space.
+          </p>
+        </ContentBox>
+        <ContentBox>
+          <h2>Menschen</h2>
+          <p>
+            Im Space triffst du die unterschiedlichsten Menschen.
+            Künstler*innen, Programmierer*innen, Lehrende und Lernende.
+          </p>
+          <div className={styles.team}>
+            <div>
+              <img
+                src={penny}
+                alt="Avatar-Bild von Patricia Ennebach"
+                title="Patricia Ennenbach"
+                className={styles.teamAvatar}
+              />
+              <p className={styles.teamName}>Penny</p>
+              <p>
+                Softwareentwicklerin. Baut datenjournalistische Formate,
+                Chatbots und Webanwendungen. Und viel zu wenig Drachen.
+                Engagiert sich für FrauenNon-BinaryTransInter und Computer Kram
+                (Fuck Cologne), Code4kids und Open Data.
+              </p>
+            </div>
+            <div>
+              <img
+                src={mop}
+                alt="Avatar-Bild von Andreas Streichardt"
+                title="Andreas Streichardt"
+                className={styles.teamAvatar}
+              />
+              <p className={styles.teamName}>Mop</p>
+              <p>
+                Freiberuflicher Softwareentwickler, Scrum Master, Kalauer as a
+                Service. Mag schnelle Ergebnisse und Prozessautomatisierung.
+                Fahrradkette statt Blockchain.
+              </p>
+            </div>
+            <div>
+              <img
+                src={christine}
+                alt="Avatar-Bild von Christine Gotthardt"
+                title="Christine Gotthardt"
+                className={styles.teamAvatar}
+              />
+              <p className={styles.teamName}>Christine</p>
+              <p>
+                Softwareentwicklerin. Zweite Heimat zwischen Indien und
+                Sizilien. Begeistert von agilen Arbeitsprozessen, netten
+                Kolleg*innen, Frontend, lernt die Begeisterung fürs Backend und
+                Geduld.
+              </p>
+            </div>
+            <div>
+              <img
+                src={marcus}
+                alt="Avatar-Bild von Marcus Weiner"
+                title="Marcus Weiner"
+                className={styles.teamAvatar}
+              />
+              <p className={styles.teamName}>Marcus</p>
+              <p>
+                Freier Softwareentwickler. Begeistert von guter Infrastruktur,
+                Kollaboration und pragmatischen Problemlösungen. Neue Ansätze
+                fürs Arbeiten fördern die Lebensqualität.
+              </p>
+            </div>
+          </div>
+          <Button label="Lern uns kennen!" url="https://google.com" />
         </ContentBox>
       </Layout>
     </GenerativeStarBackground>
