00386ec7bb7ad01e1405cb6700162418bebdae38
Marcus Weiner <marcus.weiner@gmail.com>
2022-05-10 22:39:17 +0200

    Render blog posts with layout

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,13 +2,18 @@ import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 
+import { Layout } from "../components/layout/layout";
+import { ContentBox, MODE } from "../components/content-box/content-box";
+
 /** @type {React.FC<{ data: any }>} */
 const BlogPost = (props) => {
   return (
-    <>
+    <Layout>
       <h1>{props.data.mdx.frontmatter.title}</h1>
-      <MDXRenderer>{props.data.mdx.body}</MDXRenderer>
-    </>
+      <ContentBox mode={MODE.Full}>
+        <MDXRenderer>{props.data.mdx.body}</MDXRenderer>
+      </ContentBox>
+    </Layout>
   );
 };
 
