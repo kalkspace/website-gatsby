20f156ee494e8d5ed70e1b8d4dbe4ce50e163e81
Marcus Weiner <marcus.weiner@gmail.com>
2022-05-10 22:38:01 +0200

    Apply star background on all layouted pages

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,19 +2,22 @@ import React from "react";
 
 import * as styles from "./layout.module.css";
 
+import { GenerativeStarBackground } from "../stars/stars";
 import { Header } from "../header/header";
 import { Footer } from "../footer/footer";
 
 /** @type {React.FC<{ heroImg?: React.ReactNode }>} */
 export const Layout = ({ children, heroImg }) => {
   return (
-    <div className={styles.layout}>
-      <div className={styles.container}>
-        <Header />
-        {heroImg && <div className={styles.hero}>{heroImg}</div>}
-        <main className={styles.main}>{children}</main>
+    <GenerativeStarBackground>
+      <div className={styles.layout}>
+        <div className={styles.container}>
+          <Header />
+          {heroImg && <div className={styles.hero}>{heroImg}</div>}
+          <main className={styles.main}>{children}</main>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </GenerativeStarBackground>
   );
 };
