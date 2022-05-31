97f621492631d17a729e94851e6801090437c3df
Christine <christine@brauchbarkeit.de>
2022-05-31 21:54:35 +0200

    Add event page

---
+++ src/pages/events.js
@@ -0,0 +1,74 @@
+import * as React from "react";
+
+import { Layout } from "../components/layout/layout";
+import { GenerativeStarBackground } from "../components/stars/stars";
+import { ContentBox } from "../components/content-box/content-box";
+import { TitleBox } from "../components/title-box/title-box";
+
+const EventPage = () => {
+  const [events, setEvents] = React.useState();
+
+  React.useEffect(() => {
+    fetch("https://discuss.kalk.space/discourse-post-event/events.json")
+      .then(function (eventsResponse) {
+        return eventsResponse.json();
+      })
+      .then(function (result) {
+        const futureEvents = result.events.filter(function (event) {
+          {
+            /* use yesterday's date for comparison so we don't filter out currently running events */
+          }
+          const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000);
+          return new Date(event.starts_at) > yesterday;
+        });
+
+        futureEvents.sort(function (a, b) {
+          if (a.starts_at < b.starts_at) {
+            return -1;
+          } else if (a.starts_at > b.starts_at) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+
+        setEvents(futureEvents);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <GenerativeStarBackground>
+      <Layout>
+        <TitleBox titles={["Events"]} />
+        <ContentBox mode="Full">
+          <ul>
+            {events?.map((event) => {
+              return (
+                <li key={event.post.url}>
+                  <p>
+                    {new Date(event.starts_at).toLocaleString("de", {
+                      weekday: "short",
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                  <a href={"https://discuss.kalk.space/" + event.post.url}>
+                    {event.post.topic.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </ContentBox>
+      </Layout>
+    </GenerativeStarBackground>
+  );
+};
+
+export default EventPage;
