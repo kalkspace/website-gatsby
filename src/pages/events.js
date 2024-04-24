fb862a5374cf8ea113a582f5d2ee137eb2fa1015
Marcus Weiner <marcus.weiner@gmail.com>
2024-04-24 22:41:45 +0200

    Improve event details

--- src/pages/events.js
+++ src/pages/events.js
@@ -12,24 +12,80 @@ const icsUrl = "https://kalk.space/events.ics";
 
 export const Head = () => <title>Events | KalkSpace</title>;
 
-/** @typedef {{ starts_at: number, post: { url: string, topic: { title: string } } }} Event */
+/** @typedef {{ starts_at: number, category_id: number, post: { url: string, topic: { title: string } } }} Event */
+/** @typedef {Event & { category: Category | undefined }} RichEvent */
+/** @typedef {{ events: Event[] }} EventsResponse */
+
+/** @typedef {{ id: number, name: string, color: string, subcategory_list?: Category[] }} Category */
+/** @typedef {{ category_list: { categories: Category[] } }} CategoriesResponse */
+
+/**
+ * @param {Category} category
+ * @returns {Category[]}
+ */
+const flattenCategories = (category) => {
+  return [
+    category,
+    ...(category.subcategory_list?.flatMap(flattenCategories) ?? []),
+  ];
+};
+
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
+const formatISODate = (date) =>
+  `${date.getFullYear()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+
+/**
+ * @param {Date} date
+ * @returns {string}
+ */
+const formatISOTime = (date) =>
+  `${date.getHours().toString().padStart(2, "0")}:${date
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
 
 const EventPage = () => {
   const [events, setEvents] = React.useState(
-    /** @type {Event[] | null} */ (null)
+    /** @type {RichEvent[] | null} */ (null)
   );
 
   React.useEffect(() => {
-    fetch("https://discuss.kalk.space/discourse-post-event/events.json")
-      .then(function (eventsResponse) {
-        return eventsResponse.json();
-      })
-      .then(function (/** @type {{ events: Event[] }} */ result) {
-        const futureEvents = result.events.filter(function (event) {
-          // use yesterday's date for comparison so we don't filter out currently running events
-          const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000);
-          return new Date(event.starts_at) > yesterday;
-        });
+    (async () => {
+      try {
+        /** @type {[EventsResponse, CategoriesResponse]} */
+        const [
+          { events },
+          {
+            category_list: { categories },
+          },
+        ] = await Promise.all([
+          fetch(
+            "https://discuss.kalk.space/discourse-post-event/events.json"
+          ).then((r) => r.json()),
+          fetch(
+            "https://discuss.kalk.space/categories.json?include_subcategories=true"
+          ).then((r) => r.json()),
+        ]);
+
+        const categoryDB = new Map(
+          categories.flatMap(flattenCategories).map((c) => [c.id, c])
+        );
+
+        const futureEvents = events
+          .filter(function (event) {
+            // use yesterday's date for comparison so we don't filter out currently running events
+            const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000);
+            return new Date(event.starts_at) > yesterday;
+          })
+          .map((event) => ({
+            ...event,
+            category: categoryDB.get(event.category_id),
+          }));
 
         futureEvents.sort(function (a, b) {
           if (a.starts_at < b.starts_at) {
@@ -42,10 +98,10 @@ const EventPage = () => {
         });
 
         setEvents(futureEvents);
-      })
-      .catch(function (err) {
-        console.error(err);
-      });
+      } catch (e) {
+        console.error(e);
+      }
+    })();
   }, []);
 
   return (
@@ -61,17 +117,18 @@ const EventPage = () => {
       <TitleBox>Events</TitleBox>
       <div className={styles.masonry}>
         {events?.map((event, i) => {
+          const date = new Date(event.starts_at);
           return (
-            <ContentBox mode={i % 2 == 0 ? "Left" : "Right"} masonry>
-              <p>
-                {new Date(event.starts_at).toLocaleString("de", {
-                  weekday: "short",
-                  day: "2-digit",
-                  month: "2-digit",
-                  year: "numeric",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
+            <ContentBox
+              innerClassName={styles.event}
+              mode={i % 2 == 0 ? "Left" : "Right"}
+              masonry
+            >
+              <p
+                className={styles.category}
+                style={{ borderColor: `#${event.category?.color}` }}
+              >
+                {event.category?.name}
               </p>
               <a
                 className={styles.title}
@@ -79,6 +136,22 @@ const EventPage = () => {
               >
                 {event.post.topic.title}
               </a>
+              <p className={styles.date}>
+                <time dateTime={formatISODate(date)}>
+                  {date.toLocaleString("de", {
+                    weekday: "long",
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}
+                </time>
+                <time dateTime={formatISOTime(date)}>
+                  {date.toLocaleString("de", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </time>
+              </p>
             </ContentBox>
           );
         })}
